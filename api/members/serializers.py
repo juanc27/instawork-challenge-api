680af23f44f506e2d2359810efa055ee772e767e
Juan Munoz <juanc.munozv@gmail.com>
2024-09-13 11:08:07 -0700

    Add id to the members serializer

--- api/members/serializers.py
+++ api/members/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class MemberSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Member
-        fields = ['first_name', 'last_name', 'email', 'phone', 'role', 'created_at', 'updated_at']
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone', 'role', 'created_at', 'updated_at']
